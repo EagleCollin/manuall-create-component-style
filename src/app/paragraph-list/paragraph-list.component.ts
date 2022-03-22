26905b747f1b6cff661e7f8d443f949750ea4992
EagleCollin <scoutcollin97@gmail.com>
2022-03-22 16:02:13 -0500

    Completed

---
+++ src/app/paragraph-list/paragraph-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-component-overview',
+    template: `<p class="p1">This is Paragraph 1!</p>
+    <p class="p2">This is paragraph 2</p>
+    <p class="p3">This is paragraph 3</p>`,
+    styleUrls: ['./paragraph-list.component.css']
+  })
+
+  export class ComponentOverviewComponent{
+
+  }
